970189de1f55135d9469df10ad65c7ef64049544
Joacz <joagomez.dev@gmail.com>
2023-05-24 12:59:42 -0300

    Changed urls

--- next.config.js
+++ next.config.js
@@ -4,18 +4,6 @@ module.exports = {
   async rewrites() {
     return [
       {
-        source: '/admin/subir-evento',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/formEvent',
-      },
-      {
-        source: '/admin/subir-evento/:id*',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/formEvent/:id*',
-      },
-      {
-        source: '/admin/lista-evento',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/events',
-      },
-      {
         source: '/api/event/delete/:id*',
         destination: 'http://api.escuela12neuquen.edu.ar/api/event/delete/:id*',
       },
@@ -28,25 +16,8 @@ module.exports = {
         destination: 'http://api.escuela12neuquen.edu.ar/api/event/:id*',
       },
       {
-        source: '/admin/ingresar',
-        destination: 'http://api.escuela12neuquen.edu.ar/login',
-      },
-      {
         source: '/api/publication',
         destination: 'http://api.escuela12neuquen.edu.ar/api/publication',
-      },
-      {
-        source: '/admin/subir-publicacion',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/formPublication',
-      },
-      {
-        source: '/admin/subir-publicacion/:id*',
-        destination:
-          'http://api.escuela12neuquen.edu.ar/admin/formPublication/:id*',
-      },
-      {
-        source: '/admin/lista-publicacion',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/publications',
       },
       {
         source: '/api/publication/delete/:id*',
@@ -61,10 +32,6 @@ module.exports = {
         source: '/api/image/:publicationId*',
         destination:
           'http://api.escuela12neuquen.edu.ar/api/image/:publicationId*',
-      },
-      {
-        source: '/admin',
-        destination: 'http://api.escuela12neuquen.edu.ar/admin/',
       },
       {
         source: '/api/image/:fileName*',
